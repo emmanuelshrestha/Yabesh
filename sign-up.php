6d9a477c5e2a592d4852b1e0a635790adcf64252
unknown <024neb851@sxc.edu.np>
2025-09-01 20:32:48 +0545

    db name added

--- sign-up.php
+++ sign-up.php
@@ -3,7 +3,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "your_database";
+$dbname = "signup";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
